a598e8d991c66263f7dfbbf22f1e16f87c1e4286
shiven handa <156900707+shivenhanda@users.noreply.github.com>
2026-06-29 22:19:06 +0530

    Update mongodb.js

--- backend/database/mongodb.js
+++ backend/database/mongodb.js
@@ -4,7 +4,7 @@ import dns from 'dns'
 dns.setServers(['8.8.8.8','1.1.1.1'])
 const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb+srv://shivenhanda2003_db_user:AykRBcw3hTvMWHli@cluster0.mjfajaj.mongodb.net/?appName=Cluster0", {
+        await mongoose.connect(process.env.mongoDB, {
             dbName: "UsersDB",
             serverSelectionTimeoutMS: 5000,
         })
